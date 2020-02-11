e9cc11f5f08e51d68b080f9ff89e3c3905c7a171
João Koritar <jakoritarleite@gmail.com>
2020-02-11 02:36:58 -0300

    build pass

---
+++ build/precache-manifest.1164818b8e804e88c2f09cc53371fd9a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa22c79972ed30db149fa11e606b19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca15fb1a7b42c28e662",
+    "url": "/static/css/main.716614f1.chunk.css"
+  },
+  {
+    "revision": "89192703382207e7b88a",
+    "url": "/static/js/2.1a96aaa0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1a96aaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca15fb1a7b42c28e662",
+    "url": "/static/js/main.91a51201.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "71f549c56f4c570d102c7220052cc113",
+    "url": "/static/media/Bold.71f549c5.woff2"
+  },
+  {
+    "revision": "0b5fc0eda42d69fc09a3191ab48534be",
+    "url": "/static/media/Light.0b5fc0ed.woff2"
+  },
+  {
+    "revision": "0f63abc81b6073b00635d84930b9acdc",
+    "url": "/static/media/Light.0f63abc8.woff2"
+  },
+  {
+    "revision": "f62f26a8af03bb53a0a83dd23a2fbbd7",
+    "url": "/static/media/ProductSans-Black.f62f26a8.woff"
+  },
+  {
+    "revision": "ef0c4740e420c336c3df028d8b41a105",
+    "url": "/static/media/ProductSans-BlackItalic.ef0c4740.woff"
+  },
+  {
+    "revision": "56831c947d8b6e5fdd01e9ebc4576ce2",
+    "url": "/static/media/ProductSans-Bold.56831c94.woff"
+  },
+  {
+    "revision": "bb2ed9e7721d98afd8652c6a60a31b51",
+    "url": "/static/media/ProductSans-BoldItalic.bb2ed9e7.woff"
+  },
+  {
+    "revision": "124ecceef2b99a38e7037a6cef9db5a4",
+    "url": "/static/media/ProductSans-Italic.124eccee.woff"
+  },
+  {
+    "revision": "25097357bda74912851eb5256a4699ff",
+    "url": "/static/media/ProductSans-Light.25097357.woff"
+  },
+  {
+    "revision": "7524fd2e8df80159218aaace1720b991",
+    "url": "/static/media/ProductSans-LightItalic.7524fd2e.woff"
+  },
+  {
+    "revision": "ec26aae8ba4c449f686b73576f5baf2b",
+    "url": "/static/media/ProductSans-Medium.ec26aae8.woff"
+  },
+  {
+    "revision": "255754fa1966557f15e24cca12b4aafb",
+    "url": "/static/media/ProductSans-MediumItalic.255754fa.woff"
+  },
+  {
+    "revision": "f8c351bf8263f7beefb5c4c444951389",
+    "url": "/static/media/ProductSans-Regular.f8c351bf.woff"
+  },
+  {
+    "revision": "2d7cc69ae28dee37020ddbd4a01d15d3",
+    "url": "/static/media/ProductSans-Thin.2d7cc69a.woff"
+  },
+  {
+    "revision": "833fe2e55d597e46178eba86e49fb31a",
+    "url": "/static/media/ProductSans-ThinItalic.833fe2e5.woff"
+  }
+]);
